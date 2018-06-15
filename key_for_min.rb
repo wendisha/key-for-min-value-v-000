158dbdd21c1b2ce6fcedfde320d17903e3f4caff
Wendy Calderon <wendycalderon@gmail.com>
2018-06-15 13:41:05 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,14 +4,16 @@
 def key_for_min_value(name_hash)
   #primer = 0
   #counter = name_hash.length
-  name_hash.each do |k, v|
-    if name_hash == {}
+  if name_hash == {}
       return nil
+  end
+  name_hash.each do |k, v|
+    
  # primer = v
   #  while counter < name_hash.length
    #   primer
    #   counter += 1 
     end
-  end
+ # end
  # primer
 end
